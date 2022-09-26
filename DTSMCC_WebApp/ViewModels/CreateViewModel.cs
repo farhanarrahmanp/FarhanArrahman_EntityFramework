002b6a7cc19539e55183933722c8a113e98568b3
farhan <farhanarrahman44@gmail.com>
2022-09-26 17:04:16 +0700

    initial

---
+++ DTSMCC_WebApp/ViewModels/CreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DTSMCC_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTSMCC_WebApp.ViewModels
+{
+    public class CreateViewModel
+    {
+        public IEnumerable<Department> Departments { get; set; }
+        public IEnumerable<Division> Divisions { get; set; }
+    }
+}
